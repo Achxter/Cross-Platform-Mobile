017b6698213eae78d122d2291ed8155ab3b90b6b
Achxter <hansphilemon@gmail.com>
2024-09-18 10:23:27 +0700

    done

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+
+const db = require('./assets/data.json');
 
 export default function App() {
+  const renderItem = ({ item }) => (
+    <View style={styles.container}>
+      <Image style={{
+        width: 100,
+        height: 100
+      }} source={{ uri: item.photo_url }} />
+      <Text>{item.name}</Text>
+      <Text>{item.email}</Text>
+      <View style={{
+        width: "100%", backgroundColor: 'white', height:
+          3,
+      }} />
+    </View>
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={{
+        width: 100,
+        height: 100
+      }} source={require('./assets/hans.jpg')} />
+      <Text>Hans Philemon Limanza</Text>
+      <Text>hans.philemon@student.umn.ac.id</Text>
+      <FlatList
+        data={db}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
