25754a2dd5d14604a2b429726b77f3bcd70ea0a4
Achxter <hansphilemon@gmail.com>
2024-09-21 09:53:25 +0700

    done

--- App.js
+++ App.js
@@ -1,32 +1,26 @@
+import React from 'react';
 import { StyleSheet, View, Image, ScrollView } from 'react-native';
 import userData from "./data.json"
 import styles from "./App.styles"
-import { PaperProvider, useTheme, Avatar, Button, Card, Text } from 'react-native-paper';
-import { red100 } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
+import { Provider as PaperProvider, useTheme, Avatar, Button, Card, Text, Badge, Banner, IconButton } from 'react-native-paper';
 
 export default function App() {
   const theme = useTheme();
+  const [visible, setVisible] = React.useState(true);
 
   return (
     <PaperProvider>
       <ScrollView>
         {userData.map((users) => {
           return (
-            <View style={styles.container} key={users.name}>
+            <Card style={styles.card} key={users.name} mode='elevated'>
               <Card.Title
-              style={red}
+                style={{ gap: 20, }}
                 title={users.name}
                 subtitle={users.email}
-                left={(props) => <Avatar.Image size={24} source={{ uri: users.photo_url }} />}
+                left={(props) => <Avatar.Image size={48} source={{ uri: users.photo_url }} />}
               />
-              {/* <View style={styles.card}>
-                <Image source={{ uri: users.photo_url }} style={styles.avatar} />
-                <View style={styles.boldText}>
-                  <Text style={styles.boldText}>{users.name}</Text>
-                  <Text>{users.email}</Text>
-                </View>
-              </View> */}
-            </View>
+            </Card>
           )
         })}
       </ScrollView>
